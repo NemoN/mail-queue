c959ff0d4de84df9e29d7767c8312619d2ddf6f2
Thomas Oeding <thomas.oeding@gmail.com>
2017-07-19 21:24:31 +0200

    add senderName, receiverName

--- src/Schema.php
+++ src/Schema.php
@@ -4,7 +4,7 @@ class Schema extends DatabaseBundle{
 
     public function create(){
         $sql = "CREATE TABLE IF NOT EXISTS `".$this->config->table."` (";
-        $sql .= "id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,sender VARCHAR(255) NOT NULL,receiver VARCHAR(255) NOT NULL,bcc VARCHAR(255),subject VARCHAR(255) NOT NULL,content TEXT NOT NULL,is_sent TINYINT DEFAULT '0',has_error TINYINT DEFAULT '0',error_message VARCHAR(255),try_again TINYINT DEFAULT '0',send_after DATETIME,sent_at DATETIME,created_at DATETIME NOT NULL,updated_at DATETIME NOT NULL);";
+        $sql .= "id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,sender VARCHAR(255) NOT NULL,senderName VARCHAR(255),receiver VARCHAR(255) NOT NULL,receiverName VARCHAR(255),bcc VARCHAR(255),subject VARCHAR(255) NOT NULL,content TEXT NOT NULL,is_sent TINYINT DEFAULT '0',has_error TINYINT DEFAULT '0',error_message VARCHAR(255),try_again TINYINT DEFAULT '0',send_after DATETIME,sent_at DATETIME,created_at DATETIME NOT NULL,updated_at DATETIME NOT NULL);";
         $this->conn->exec($sql);
     }
 
